bc27ebd8529225984eceee2393d7bd74877ff10c
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-04-28 11:27:19 -0500

    adding boilerplate code for views

--- LuckyPetAdoption/application/web/source/lucky-pet-adoption/src/App.js
+++ LuckyPetAdoption/application/web/source/lucky-pet-adoption/src/App.js
@@ -2,6 +2,12 @@ import './App.css'
 import { Container } from 'react-bootstrap'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../views/Home'
+import About from './views/About'
+import AllPets from './views/AllPets'
+import Cart from './views/Cart'
+import Admin from './views/Admin'
+import Checkout from './views/Checkout'
+import CheckoutSummary from './views/CheckoutSummary'
 
 function App() {
   return (
