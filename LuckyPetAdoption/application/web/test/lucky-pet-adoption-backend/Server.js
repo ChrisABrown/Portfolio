d28bfb81057668b5f8f096b493303e5b8ee77436
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-04-29 15:26:57 -0500

    adding static data folder to represent data

--- LuckyPetAdoption/application/web/test/lucky-pet-adoption-backend/Server.js
+++ LuckyPetAdoption/application/web/test/lucky-pet-adoption-backend/Server.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const products = require('./data/products')
 
-const port = process.env.PORT
+const port = 5000
 
 const app = express()
 
-app.listen(port, console.log('Server running on port' + port))
+app.get('/', (req, res) => {
+  res.send('API up and running...')
+})
+
+app.get('/api/store-items', (req, res) => {
+  res.json(products)
+})
+
+app.listen(port, console.log('Server running on port ' + port))
