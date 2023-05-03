106b877a7f2e7d42516ca67f9fdf7b7455cf099a
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-05-02 20:12:33 -0500

    (Backend): adjustments to server code

--- LuckyPetAdoption/application/web/test/lucky-pet-adoption-backend/Server.js
+++ LuckyPetAdoption/application/web/test/lucky-pet-adoption-backend/Server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const products = require('./data/products')
 
 const port = 5000
 
 const app = express()
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('API up and running...')
