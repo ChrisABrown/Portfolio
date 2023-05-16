bbe3b66a3a6f85ee7ff6f997adde56a7da98b326
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-05-15 19:42:30 -0500

    starting on tests for basic app

---
+++ BasicFullStack/backend/src/test/java/com/portfolio/FullStack/models/UserTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.FullStack.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.UserCredentialsDataSourceAdapter;
+
+public class UserTests {
+
+    @Test
+    void should_RetTrue_When_Username_Empty(){
+        Object user;
+        
+    }
+}
