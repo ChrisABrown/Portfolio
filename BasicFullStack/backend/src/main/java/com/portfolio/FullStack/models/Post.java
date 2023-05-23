725451cd99c72f4489d1effc53eabd05418b5737
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-05-23 17:23:22 -0500

    (Backend): started adding notations for SQL integration needs fine-tuning

--- BasicFullStack/backend/src/main/java/com/portfolio/FullStack/models/Post.java
+++ BasicFullStack/backend/src/main/java/com/portfolio/FullStack/models/Post.java
@@ -1,6 +1,9 @@
 package com.portfolio.FullStack.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,25 +13,31 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Posts")
 public class Post {
 
     String postId;
+    @Id
     User user;
     LocalDateTime createdOn;
     LocalDateTime updatedOn;
     String body;
     Comment[] comments;
 
-    static @NotNull String usingRandomUUID(){
+    static @NotNull String usingRandomUUID() {
         UUID randomUUID = UUID.randomUUID();
-        return randomUUID.toString().replaceAll("_", "").substring(0,6);
+        return randomUUID.toString().replaceAll("_", "").substring(0, 6);
     }
+
     public boolean isEmpty() {
         return body == null || body.equals("");
     }
 
 
-    public void setPostId(){
+    public void setPostId() {
         this.postId = usingRandomUUID();
     }
 }
