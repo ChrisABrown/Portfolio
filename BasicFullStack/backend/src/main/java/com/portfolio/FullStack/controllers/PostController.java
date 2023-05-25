6003a6688e043a43697d850d5a99e34256d84420
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-05-25 12:46:05 -0500

    (Backend): Post Controller - need to add autowiring for users to get posts by user

--- BasicFullStack/backend/src/main/java/com/portfolio/FullStack/controllers/PostController.java
+++ BasicFullStack/backend/src/main/java/com/portfolio/FullStack/controllers/PostController.java
@@ -1,4 +1,32 @@
 package com.portfolio.FullStack.controllers;
 
+
+import com.portfolio.FullStack.models.Post;
+import com.portfolio.FullStack.models.User;
+import com.portfolio.FullStack.repositories.UserRepository;
+import com.portfolio.FullStack.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/v1/posts")
 public class PostController {
+
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<Object> getPostsByUserId(){
+        List<Post> postsByUserId = postService.getAllPosts();
+
+    }
 }
--- BasicFullStack/backend/src/main/java/com/portfolio/FullStack/controllers/UserController.java
+++ BasicFullStack/backend/src/main/java/com/portfolio/FullStack/controllers/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @CrossOrigin("http://localhost:3000")
 @RequestMapping("/api/v1/users")
@@ -15,6 +17,26 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getUserByUserId(@PathVariable String userId) {
+        Optional<User> userById = userService.getOneUser(userId);
+
+        if (userById.isPresent() && userById.get().getUserId().equals(userId)) {
+            return new ResponseEntity<>(new AppResponse(HttpStatus.OK.value(),
+                    "Found user with the userId: " + userId,
+                    true,
+                    userById),
+                    HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new AppResponse(HttpStatus.NOT_FOUND.value(),
+                "Could not find user with userId: " + userId,
+                false,
+                null),
+                HttpStatus.NOT_FOUND);
+
+    }
 
     @PostMapping
     public ResponseEntity<Object> createUser(@RequestBody User userDetails) {
@@ -27,17 +49,16 @@ public class UserController {
     }
 
     @PutMapping("/{userId}")
-    public ResponseEntity<Object> updateUser(@PathVariable String userId, @RequestBody User userDetails){
+    public ResponseEntity<Object> updateUser(@PathVariable String userId, @RequestBody User userDetails) {
         User updatedUser = userService.updateUserInfo(userId, userDetails);
 
-        if(updatedUser.getIsUserIdEmpty().equals(true)){
+        if (updatedUser.getIsUserIdEmpty().equals(true)) {
             return new ResponseEntity<>(new AppResponse(HttpStatus.NOT_FOUND.value(),
                     "UserId for User could not be found",
                     false,
                     null),
                     HttpStatus.NOT_FOUND);
-        }
-        else if(!updatedUser.getUserId().equals(userId)){
+        } else if (!updatedUser.getUserId().equals(userId)) {
             return new ResponseEntity<>(new AppResponse(HttpStatus.NOT_FOUND.value(),
                     "UserId for User does not match UserId found",
                     false,
@@ -50,6 +71,28 @@ public class UserController {
                 true,
                 updatedUser),
                 HttpStatus.FOUND);
+
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteUserByUserId(@PathVariable String userId) {
+
+        Optional<User> userToDelete = userService.getOneUser(userId);
+
+        if (userToDelete.isPresent() && userId.equals(userToDelete.get().getUserId())) {
+            userService.deleteUser(userToDelete.get().getUserId());
+            return new ResponseEntity<>(new AppResponse(HttpStatus.OK.value(),
+                    "Deleted user with userId: " + userId,
+                    true,
+                    null),
+                    HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new AppResponse(HttpStatus.NOT_FOUND.value(),
+                "Could not find user with userId: " + userId,
+                false,
+                null),
+                HttpStatus.NOT_FOUND);
 
     }
 }
