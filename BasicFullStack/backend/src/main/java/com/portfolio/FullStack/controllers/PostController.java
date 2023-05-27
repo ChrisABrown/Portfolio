d7d350ba8b437105b53d2eacf80149d28930e288
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-05-26 22:03:40 -0500

    (Backend): Post Controller - finished adding methods for Post Controller

--- BasicFullStack/backend/src/main/java/com/portfolio/FullStack/controllers/PostController.java
+++ BasicFullStack/backend/src/main/java/com/portfolio/FullStack/controllers/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,10 +37,49 @@ public class PostController {
 
     }
 
-    @GetMapping()
+    @GetMapping("/{userId}/{postId}")
+    public ResponseEntity<Object> getSinglePostByUserIdAndPostId(@PathVariable String userId,
+                                                                 @RequestBody @PathVariable String postId) {
+        Optional<User> postingUser = userService.getOneUser(userId);
+        ArrayList<Post> posts = postingUser.get().getPosts();
+        for (Post post : posts) {
+            if (post.getUser().getUserId().equals(userId) || post.getPostId().equals(postId)) {
+                return new ResponseEntity<>(new AppResponse(HttpStatus.OK.value(),
+                        "Found one post matching the postId: " + postId + " for the User with userId: " + userId,
+                        true,
+                        post.getBody()),
+                        HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(new AppResponse(HttpStatus.NOT_FOUND.value(),
+                "Could not find a post matching the postId: " + postId + " for the User with userId: " + userId,
+                false,
+                null),
+                HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<Object> updatePost(@PathVariable String postId,
+                                             @RequestBody Post postDetails) {
+        Post updatedPost = postService.updatePost(postId, postDetails);
+
+        if (updatedPost.isEmpty() && updatedPost.getPostId().isEmpty()) {
+            return new ResponseEntity<>(new AppResponse(HttpStatus.NO_CONTENT.value(),
+                    "Post cannot be empty",
+                    false,
+                    null),
+                    HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(new AppResponse(HttpStatus.OK.value(),
+                "Post updated",
+                true,
+                updatedPost),
+                HttpStatus.OK);
+    }
 
     @PostMapping("/{userId}")
-    public ResponseEntity<Object> createPost(@PathVariable String userId, @RequestBody Post postDetails) {
+    public ResponseEntity<Object> createPost(@PathVariable String userId,
+                                             @RequestBody Post postDetails) {
         Optional<User> postingUser = userService.getOneUser(userId);
 
         if (postingUser.isPresent() && userId.equals(postDetails.getUser().getUserId())) {
@@ -58,5 +98,28 @@ public class PostController {
                     null),
                     HttpStatus.NOT_FOUND);
         }
+    }
+
+    @DeleteMapping("/{userId}/{postId}")
+    public ResponseEntity<Object> deletePostByUserIdAndPostId(@PathVariable String userId,
+                                                              @RequestBody @PathVariable String postId) {
+        Optional<User> foundUser = userService.getOneUser(userId);
+        ArrayList<Post> posts = foundUser.get().getPosts();
+
+        for (Post post : posts) {
+            if (post.getPostId().equals(postId)) {
+                postService.deletePost(postId);
+                return new ResponseEntity<>(new AppResponse(HttpStatus.OK.value(),
+                        "Deleted post with postId: " + postId,
+                        true,
+                        null),
+                        HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(new AppResponse(HttpStatus.NOT_FOUND.value(),
+                "Could not find post matching postId: " + postId,
+                false,
+                postId),
+                HttpStatus.NOT_FOUND);
     }
 }
