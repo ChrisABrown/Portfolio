95f2fc2c8bca96e56d0f07200ee4f024ff478257
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-05-30 11:38:15 -0500

    (Frontend): minor code changes

--- BasicFullStack/frontend/src/index.js
+++ BasicFullStack/frontend/src/index.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
+import App from 'App.js'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: {},
   },
 ])
 
